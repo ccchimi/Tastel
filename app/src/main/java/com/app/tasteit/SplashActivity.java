ddd637a009e97efc99608314a6abc00626486647
Franco Schimizzi <franco.schimizzi@davinci.edu.ar>
2025-09-24 16:20:08 -0300

    fix:
    corregir SplashActivity (uso de Handler con Looper y navegacion a MainActivity)

--- app/src/main/java/com/app/tasteit/SplashActivity.java
+++ app/src/main/java/com/app/tasteit/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
@@ -11,9 +12,9 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Espera 2 segundos y luego abre el Login
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        // Espera 2 segundos y luego abre MainActivity (por ahora)
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         }, 2000);

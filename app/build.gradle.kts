89d83ca357500e855e84fe91dcc2ff09508c7f7c
Franco Schimizzi <franco.schimizzi@davinci.edu.ar>
2025-09-29 17:40:01 -0300

    update:
    adicion de dependencia com.google.code.gson:gson:2.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -44,4 +43,5 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
